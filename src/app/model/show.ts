4b62a4dc18ad6c8cfda606dc93619cb4a65fa43c
GenocideSoul <ikadox3@gmx.de>
2021-10-07 08:47:25 +0200

    1 file modified

--- src/app/model/show.ts
+++ src/app/model/show.ts
@@ -1,10 +1,3 @@
 export class Shows {
   constructor(public id: number, public title: string) {}
-  ersetzt;
-  public id: number;
-  public title: string;
-  constructor(id: number, title: string) {
-    this.id = id;
-    this.title = title;
-  }
 }
