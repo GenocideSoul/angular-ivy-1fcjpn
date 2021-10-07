f9f3ff45c2424abe748f7d82e3b37416bd14dd9d
GenocideSoul <ikadox3@gmx.de>
2021-10-07 09:10:50 +0200

    1 file modified

--- src/app/components/show-list/show-list.component.ts
+++ src/app/components/show-list/show-list.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Show } from '//model/show';
 
 @Component({
   selector: 'app-show-list',
   templateUrl: './show-list.component.html',
-  styleUrls: ['./show-list.component.css']
+  styleUrls: ['./show-list.component.css'],
 })
 export class ShowListComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
